466abb221c5848426dea911032ea33e4d1610867
Chris <chrissbolding@gmail.com>
2014-01-23 06:42:23 +1100

    Update PlayerKick.java

--- src/me/shawshark/kitpvp/EventListeners/PlayerKick.java
+++ src/me/shawshark/kitpvp/EventListeners/PlayerKick.java
@@ -2,6 +2,7 @@ package me.shawshark.kitpvp.EventListeners;
 
 import me.shawshark.kitpvp.main;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
@@ -16,7 +17,7 @@ public class PlayerKick implements Listener {
 	
 	@EventHandler
 	public void onKick(PlayerKickEvent e) {
-		e.setLeaveMessage(null);
+		e.setLeaveMessage(ChatColor.BLUE + "- " + e.getPlayer().getName() + " has left.");
 		m.usb.updateforeveryone();
 	}
 }
